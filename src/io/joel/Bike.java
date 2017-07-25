08d2f029a064d0f402c018a7b665b9ff3ce69752
taddeimania <jtaddei@gmail.com>
2017-07-25 09:42:57 -0400

    Created a Bike bean, integrated into runner

---
+++ src/io/joel/Bike.java
@@ -0,0 +1,19 @@
+package io.joel;
+
+public class Bike {
+    private double location;
+    private double speed;
+
+    public Bike() {
+        location = 0;
+        speed = 0;
+    }
+
+    public double getLocation() {
+        return location;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+}
---
+++ src/io/joel/Main.java
@@ -0,0 +1,10 @@
+package io.joel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Bike bike = new Bike();
+        System.out.println(bike.getLocation());
+        System.out.println(bike.getSpeed());
+    }
+}
