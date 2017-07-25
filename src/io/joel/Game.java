b86fc8a6bddd03bd8c5698047de712402c07973a
taddeimania <jtaddei@gmail.com>
2017-07-25 11:55:40 -0400

    Got the game class working!

--- src/io/joel/Game.java
+++ src/io/joel/Game.java
@@ -5,10 +5,35 @@ import java.util.List;
 
 public class Game {
     List<Bike> bikes = new ArrayList<>();
+    private int finishLine = 100;
 
     public void AddBike() {
         Bike newBike = new Bike();
         bikes.add(newBike);
+    }
+
+    public void StartRace() {
+        boolean raceActive = true;
+        List<Bike> finishers = new ArrayList<>();
+        while (raceActive) {
+            moveBikes();
+            for (Bike bike : bikes) {
+                if (bike.getLocation() >= finishLine){
+                    finishers.add(bike);
+                    raceActive = false;
+                }
+            }
+        }
+        System.out.println("WE HAVE A WINNNNNNER!");
+        if (finishers.size() > 1) {
+            // Photo Finish
+        } else {
+            finishers.get(0).report();
+        }
+    }
+
+    public void setFinishLine(int newLocation) {
+        finishLine = newLocation;
     }
 
     public void AddBike(int count) {
--- src/io/joel/Main.java
+++ src/io/joel/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         Game game = new Game();
+        game.setFinishLine(1000);
         game.AddBike(5);
-        game.moveBikes(10);
+        game.StartRace();
         game.BikeReport();
 
     }
